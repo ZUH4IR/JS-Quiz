b9b8b80d946acce8594cd3dc097b4ff8da91ac8a
ZUH4Ir <zuhairlakhani@gmail.com>
2022-11-28 12:41:15 -0600

    Elaborated comments

--- script.js
+++ script.js
@@ -25,10 +25,9 @@ let textEl = document.getElementById("status-text")
 let nextQuesBtn = document.querySelector('#next-ques')
 let ans = allQuestions[currQues].correct
 let choiceDisable = false
+let buttons = document.getElementsByClassName("choice");
 
-const buttons = document.getElementsByClassName("choice");
-
-function initialize(){ // Set Values to Question & Answer Choices
+function initialize(){ // Set Values to Question, Answer Choices, Enable Buttons
     ans = allQuestions[currQues].correct
     document.getElementById("q").innerHTML = allQuestions[currQues].question
     document.getElementById("A-btn").textContent = allQuestions[currQues].A
@@ -42,7 +41,7 @@ function initialize(){ // Set Values to Question & Answer Choices
 
 initialize() // Question 1
 
-function displayEnd(){ // Reveal Next Button and Status Function
+function displayEnd(){ // Reveal Next Button and Status Function, Disable Buttons
     nextQuesBtn.style.display = "inline";
     textEl.style.display = "block"
     for (let i = 0; i < buttons.length; i++) {
@@ -66,7 +65,7 @@ function checkAnswer(x){ // Check Answer Function
     displayEnd()
 }
 
-nextQuesBtn.addEventListener("click", () => { // next question button
+nextQuesBtn.addEventListener("click", () => { // Next Question Button
     hideEnd()
     if(currQues >= allQuestions.length - 1) {
         currQues = 0
@@ -77,7 +76,7 @@ nextQuesBtn.addEventListener("click", () => { // next question button
     initialize()
 })
 
-document.querySelector('#reset-quiz').addEventListener("click", () => { // reset question button
+document.querySelector('#reset-quiz').addEventListener("click", () => { // Reset Question Button
     currQues = 0
     initialize()
 })
